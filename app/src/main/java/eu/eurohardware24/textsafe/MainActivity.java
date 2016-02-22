bbc24ffb21c7a15a8b24629efdf928a73185634f
Dominic Barra <dombarra@eurohardware24.eu>
2016-02-22 20:07:20 +0100

    First Version

--- app/src/main/java/eu/eurohardware24/textsafe/MainActivity.java
+++ app/src/main/java/eu/eurohardware24/textsafe/MainActivity.java
@@ -2,45 +2,20 @@ package eu.eurohardware24.textsafe;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.PopupMenu;
-import android.widget.PopupMenu.OnMenuItemClickListener;
-import android.widget.Toast;
+import android.widget.EditText;
 
 
-
-public class MainActivity extends  Activity implements OnMenuItemClickListener {
+public class MainActivity extends Activity {
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btn_click).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
-                popupMenu.setOnMenuItemClickListener(MainActivity.this);
-                popupMenu.inflate(R.menu.safepopup);
-                popupMenu.show();
-            }
-        });
+        EditText text = (EditText) findViewById(R.id.editText);
+        text.setVisibility(View.GONE);
     }
 
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item_comedy:
-                Toast.makeText(this, "Comedy Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item_movies:
-                Toast.makeText(this, "Movies Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item_music:
-                Toast.makeText(this, "Music Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-        }
-    }
+
 }
